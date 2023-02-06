fb1d702daa57b90919aef3592c197c08ae6df7e8
Víctor Merino <victor.cornella.86@gmail.com>
2023-02-06 12:41:40 +0100

    Add startServer file

---
+++ src/server/startServer.ts
@@ -0,0 +1,18 @@
+import chalk from "chalk";
+import debugCreator from "debug";
+import app from "./app.js";
+
+const debug = debugCreator("compraLista:server:startServer");
+
+const startServer = async (port: number) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.blue(`Server listening on http://localhost:${port}`));
+      resolve(true);
+    });
+
+    server.on("error", (error) => {
+      debug(chalk.red("Error connecting to the server", error.message));
+      reject(error);
+    });
+  });
