f8c9f7f59ddd3fc19f53dce5be6d6b8fbf9db78a
Víctor Merino <victor.cornella.86@gmail.com>
2023-02-06 14:36:07 +0100

    Fix eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,16 +18,10 @@ module.exports = {
         "@typescript-eslint/no-empty-function": "off",
       },
     },
-    {
-      files: ["src/**/models/**/*.ts"],
-      rules: { "@typescript-eslint/naming-convention": "off" },
-    },
   ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {
-    "no-implicit-coercion": "off",
-  },
+  rules: {},
 };
