382e29e1c65f769d5883d2af9ea6b3779b60caf9
Víctor Merino <victor.cornella.86@gmail.com>
2023-03-03 07:35:05 +0100

    Add CustomError class unit test

--- src/utils/CustomError.ts
+++ src/utils/CustomError.ts
@@ -1,7 +1,7 @@
 class CustomError extends Error {
   constructor(
     message: string,
-    public statusCode: number,
+    public status: number,
     public publicMessage: string
   ) {
     super(message);
