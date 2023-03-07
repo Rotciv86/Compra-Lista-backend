624449e7ad1ceed93cc8ea230a229d0083330dc2
Víctor Merino <victor.cornella.86@gmail.com>
2023-03-07 11:12:48 +0100

    Fix unnecessary exclusion rule in types of eslint

--- src/server/routes/usersRouters.test.ts
+++ src/server/routes/usersRouters.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import request from "supertest";
