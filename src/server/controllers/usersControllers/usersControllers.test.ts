f79d3afd8862bff28b9cb92ada9ff6d1b526eff6
Víctor Merino <victor.cornella.86@gmail.com>
2023-03-29 22:08:53 +0200

    Add loginUser controller automatic unitary test

--- src/server/controllers/usersControllers/usersControllers.test.ts
+++ src/server/controllers/usersControllers/usersControllers.test.ts
@@ -1,9 +1,11 @@
 import type { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../../../database/models/User.js";
 import type { RegisterUserData } from "../../types.js";
-import { registerUser } from "./usersControlers.js";
+import { loginUser, registerUser } from "./usersControlers.js";
+import CustomError from "../../../utils/CustomError.js";
 
 const res: Partial<Response> = {
   status: jest.fn().mockReturnThis(),
@@ -57,6 +59,71 @@ describe("Given the registerUser controller", () => {
       User.create = jest.fn().mockRejectedValue(error);
 
       await registerUser(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+});
+
+describe("Given the loginUser controller", () => {
+  const req: Partial<Request> = {
+    body: { username: "Juan", password: "1234" },
+  };
+
+  const res: Partial<Response> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const mockedUser = {
+    _id: "2",
+    username: "paco",
+    password: "1254",
+  };
+  const next = jest.fn() as Partial<NextFunction>;
+
+  describe("When it receives a request with the username 'Juan' with the password 1234 in its body", () => {
+    test("Then it should call the status method of the response  with value 200 and json method with a token", async () => {
+      jwt.sign = jest.fn().mockReturnValueOnce("1234");
+
+      bcryptjs.compare = jest.fn().mockResolvedValueOnce(true);
+
+      User.findOne = jest.fn().mockResolvedValue(mockedUser);
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({ token: "1234" });
+    });
+  });
+
+  describe("When it receives a request with a username 'admin' and the user is not found", () => {
+    test("Then the function next should be invoked with a CustomError", async () => {
+      const error = new CustomError(
+        "Username not found",
+        401,
+        "Wrong credentials"
+      );
+
+      User.findOne = jest.fn().mockResolvedValueOnce(null);
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe("When it receives a request with a username 'admin' and password 'admin', and the password is not correct", () => {
+    test("Then the function next should be invoked with a CustomError", async () => {
+      const error = new CustomError(
+        "Incorrect password",
+        401,
+        "Wrong credentials"
+      );
+
+      User.findOne = jest.fn().mockResolvedValueOnce(mockedUser);
+      bcryptjs.compare = jest.fn().mockResolvedValue(false);
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
 
       expect(next).toHaveBeenCalledWith(error);
     });
